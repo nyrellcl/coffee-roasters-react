1a493f9d3ce891a63f3e19a58d090bee0f359778
nyrellcl <nyrell.leonor2000@gmail.com>
2022-12-23 10:49:04 +0800

    edit

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,12 +1,8 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../assets/shared/desktop/logo.svg"
-<<<<<<< HEAD
 import iconHam from "../assets/shared/mobile/icon-hamburger.svg"
 import iconClose from "../assets/shared/mobile/icon-close.svg"
-=======
-import { FaBars, FaTimes } from 'react-icons/fa'
->>>>>>> 67dd6e8d73de20e49bb67335f42055dd9a7e0239
 import "./Nav.css"
 
 function Nav() {
@@ -22,21 +18,12 @@ function Nav() {
         </Link>
         
         <nav className='menu-icon' onClick={handleClick}>
-<<<<<<< HEAD
             {click ? {iconHam} : {iconClose}}
         </nav>
         <ul className={click ? 'nav-list active' : 'nav-list'}>
           <li><Link to="/coffee-roasters-react" onClick={closeMobileMenu}>Home</Link></li>
           <li><Link to="/about" onClick={closeMobileMenu}>About</Link></li>
           <li><Link to="/plan" onClick={closeMobileMenu}>Plan</Link></li>
-=======
-        {click ? <FaTimes /> : <FaBars />}
-        </nav>
-        <ul className={click ? 'nav-list active' : 'nav-list'}>
-          <li><Link to="/coffee-roasters-react" onClick={closeMobileMenu}>Home</Link></li>
-          <li><Link to="/about" onClick={closeMobileMenu}>About Us</Link></li>
-          <li><Link to="/plan" onClick={closeMobileMenu}>Create Your Plan</Link></li>
->>>>>>> 67dd6e8d73de20e49bb67335f42055dd9a7e0239
         </ul>
     </header>
   )
--- src/pages/home/MainHome.js
+++ src/pages/home/MainHome.js
@@ -1,26 +1,17 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
 import HeroHome from './HeroHome'
 import CollectionHome from './Collection/CollectionHome'
 import ServicesHome from './Services/ServicesHome'
 import ProcessHome from './Process/ProcessHome'
->>>>>>> 67dd6e8d73de20e49bb67335f42055dd9a7e0239
 
 
 function MainHome() {
   return (
     <>
-<<<<<<< HEAD
-    <div>
-        <h1>MainHome</h1>
-    </div>
-=======
    <HeroHome/>
    <CollectionHome/>
    <ServicesHome/>
    <ProcessHome/>
->>>>>>> 67dd6e8d73de20e49bb67335f42055dd9a7e0239
     </>
 
   )
