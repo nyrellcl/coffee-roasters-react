dcef38c2dedf6b8b6bddfd75f5640aa100bdd1d0
nyrellcl <nyrell.leonor2000@gmail.com>
2023-01-23 05:01:18 -0800

    added line decor for desktop

--- src/pages/home/Process/ProcessHome.js
+++ src/pages/home/Process/ProcessHome.js
@@ -4,10 +4,17 @@ import ProcessTemplate from './ProcessTemplate'
 import {Link} from 'react-router-dom'
 import "../../../Index-SCSS/index.css"
 
-function ProcessHome(processBg) {
+function ProcessHome(processBg, lineDecor) {
   return (
     <section className={processBg ? 'process-section': 'process-section dark'}>
         <h4>How it works</h4>
+        <div className={lineDecor ? 'line-decor' : 'line-decor dark'}>
+          <div className='circle-container'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
          <div className='process-container'>
             <ProcessTemplate {...processOne}/>
             <ProcessTemplate {...processTwo}/>
